b00cc10ad443e0809b7a55fa2d325230512dcc50
IDOMATH <btdothemath@gmail.com>
2024-10-03 20:26:24 -0500

    Adding server

--- main.go
+++ main.go
@@ -1,7 +1,18 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
 
 func main() {
-	fmt.Println("Hello world")
+	router := http.NewServeMux()
+	server := http.Server{
+		Addr:    ":8080",
+		Handler: router,
+	}
+
+	fmt.Println("Starting on port 8080")
+	log.Fatal(server.ListenAndServe())
 }
