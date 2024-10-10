0be462cc8b93455a63fb6522dff79337807213df
IDOMATH <btdothemath@gmail.com>
2024-10-09 20:50:14 -0500

    Adding first version of array/int conversions:

--- types/tournament.go
+++ types/tournament.go
@@ -1,5 +1,7 @@
 package types
 
+import "math"
+
 type Tournament struct {
 	Name        string
 	Host        Location
@@ -18,8 +20,55 @@ const (
 	IsGirlsYouth
 )
 
-//TODO: make method for converting AgeDivision to an integer
-// and another to convert it back
+func AgeDivisionArrayToInt(s [8]bool) int {
+	var value int = 0
+	for i := range s {
+		if s[i] {
+			value += int(math.Pow(2, float64(i)))
+		}
+	}
+	return value
+}
+
+// TODO: Change this to make it more general
+func AgeDivisionIntToArray(val int) [8]bool {
+	var arr [8]bool
+	for i := 7; i >= 0; i-- {
+		if val >= 128 {
+			arr[i] = true
+			val -= 128
+		}
+		if val >= 64 {
+			arr[i] = true
+			val -= 64
+		}
+		if val >= 32 {
+			arr[i] = true
+			val -= 32
+		}
+		if val >= 16 {
+			arr[i] = true
+			val -= 16
+		}
+		if val >= 8 {
+			arr[i] = true
+			val -= 8
+		}
+		if val >= 4 {
+			arr[i] = true
+			val -= 4
+		}
+		if val >= 2 {
+			arr[i] = true
+			val -= 2
+		}
+		if val >= 1 {
+			arr[i] = true
+			val -= 1
+		}
+	}
+	return arr
+}
 
 type Location struct {
 	Name    string
