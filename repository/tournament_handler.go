b68071bc7536cc11a7b9977a17e8749edb14f253
IDOMATH <btdothemath@gmail.com>
2024-10-17 21:33:49 -0500

    Starting handle get all tournaments

--- repository/tournament_handler.go
+++ repository/tournament_handler.go
@@ -5,3 +5,7 @@ import "github.com/IDOMATH/tournament-finder/db"
 type TournamentHandler struct {
 	tournamentStore db.TournamentStore
 }
+
+func (h *TournamentHandler) HandleGetAllTournaments() {
+	tournaments := h.tournamentStore.GetAllTournaments
+}
