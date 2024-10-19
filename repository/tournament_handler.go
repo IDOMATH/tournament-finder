8c3209c41fd9f7c6f12e422a5f726772638aaf97
IDOMATH <btdothemath@gmail.com>
2024-10-18 19:10:05 -0500

    Adding template data

--- repository/tournament_handler.go
+++ repository/tournament_handler.go
@@ -1,11 +1,28 @@
 package repository
 
-import "github.com/IDOMATH/tournament-finder/db"
+import (
+	"net/http"
+
+	"github.com/IDOMATH/tournament-finder/db"
+	"github.com/IDOMATH/tournament-finder/types"
+)
 
 type TournamentHandler struct {
 	tournamentStore db.TournamentStore
 }
 
-func (h *TournamentHandler) HandleGetAllTournaments() {
-	tournaments := h.tournamentStore.GetAllTournaments
+func (h *TournamentHandler) HandleGetTournaments(w http.ResponseWriter, r *http.Request) {
+	td := types.TemplateData{
+		PageName:  "All Tournaments",
+		ObjectMap: make(map[string]interface{}),
+	}
+	tournaments, err := h.tournamentStore.GetAllTournaments()
+	if err != nil {
+		w.Write([]byte(err.Error()))
+		return
+	}
+
+	td.ObjectMap["tournaments"] = tournaments
+
+	//TODO: render template
 }
---
+++ types/template_data.go
@@ -0,0 +1,6 @@
+package types
+
+type TemplateData struct {
+	PageName  string
+	ObjectMap map[string]interface{}
+}
