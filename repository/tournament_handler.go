0850976013baf89582a8f4a48ade0b8409a39604
IDOMATH <btdothemath@gmail.com>
2024-10-21 21:45:30 -0500

    Flattening repo

--- repository/tournament_handler.go
+++ repository/tournament_handler.go
@@ -2,6 +2,7 @@ package repository
 
 import (
 	"net/http"
+	"strconv"
 
 	"github.com/IDOMATH/tournament-finder/db"
 	"github.com/IDOMATH/tournament-finder/types"
@@ -11,12 +12,12 @@ type TournamentHandler struct {
 	tournamentStore db.TournamentStore
 }
 
-func (h *TournamentHandler) HandleGetTournaments(w http.ResponseWriter, r *http.Request) {
+func (repo *Repository) HandleGetTournaments(w http.ResponseWriter, r *http.Request) {
 	td := types.TemplateData{
 		PageName:  "All Tournaments",
 		ObjectMap: make(map[string]interface{}),
 	}
-	tournaments, err := h.tournamentStore.GetAllTournaments()
+	tournaments, err := repo.TH.tournamentStore.GetAllTournaments()
 	if err != nil {
 		w.Write([]byte(err.Error()))
 		return
@@ -27,9 +28,12 @@ func (h *TournamentHandler) HandleGetTournaments(w http.ResponseWriter, r *http.
 	//TODO: render template
 }
 
-func (h *TournamentHandler) HandleGetTournamentById(w http.ResponseWriter, r *http.Request) {
-	id := r.PathValue("id")
-	tournament, err := h.tournamentStore.GetTournamentById(id)
+func (repo *Repository) HandleGetTournamentById(w http.ResponseWriter, r *http.Request) {
+	id, err := strconv.Atoi(r.PathValue("id"))
+	if err != nil {
+		w.Write([]byte("error converting ID from string to int: " + err.Error()))
+	}
+	tournament, err := repo.TH.tournamentStore.GetTournamentById(id)
 	if err != nil {
 		w.Write([]byte(err.Error()))
 		return
