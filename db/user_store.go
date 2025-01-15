8836574da641d3e1f00fcd0e509a153d844042b2
IDOMATH <btdothemath@gmail.com>
2025-01-14 21:00:41 -0600

    Adding insert user to user store

--- db/user_store.go
+++ db/user_store.go
@@ -1,6 +1,12 @@
 package db
 
-import "database/sql"
+import (
+	"context"
+	"database/sql"
+	"time"
+
+	"github.com/IDOMATH/tournament-finder/types"
+)
 
 type UserStore struct {
 	Db *sql.DB
@@ -10,4 +16,20 @@ func NewUserStore(db *sql.DB) *UserStore {
 	return &UserStore{
 		Db: db,
 	}
+}
+
+func (s *UserStore) InsertUser(user types.User) (int, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+
+	var newId int
+	statement := `insert into users (name, email, created_at) values ($1, $2, $3)`
+
+	err := s.Db.QueryRowContext(ctx, statement,
+		user.Name,
+		user.Email, time.Now()).Scan(&newId)
+	if err != nil {
+		return 0, err
+	}
+	return newId, nil
 }
