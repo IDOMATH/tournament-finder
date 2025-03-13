a8c14bbcfb5fab316cf6e554618566dddba4c384
IDOMATH <btdothemath@gmail.com>
2025-03-12 20:35:46 -0500

    Fixing typos

--- backend/repository/tournament_handler.go
+++ backend/repository/tournament_handler.go
@@ -33,7 +33,7 @@ func (repo *Repository) HandlePostTournament(w http.ResponseWriter, r *http.Requ
 	res, err := json.Marshal(id)
 	if err != nil {
 		w.WriteHeader(http.StatusInternalServerError)
-		w.Write([]byte("error marhalling json"))
+		w.Write([]byte("error marshalling json"))
 		return
 	}
 	w.WriteHeader(http.StatusCreated)
@@ -52,7 +52,7 @@ func (repo *Repository) HandlePutTournament(w http.ResponseWriter, r *http.Reque
 	res, err := json.Marshal(updatedTournament)
 	if err != nil {
 		w.WriteHeader(http.StatusInternalServerError)
-		w.Write([]byte("error marhalling json"))
+		w.Write([]byte("error marshalling json"))
 		return
 	}
 	w.WriteHeader(http.StatusOK)
@@ -90,7 +90,7 @@ func (repo *Repository) HandleGetTournamentById(w http.ResponseWriter, r *http.R
 	resTournament, err := json.Marshal(tournament)
 	if err != nil {
 		w.WriteHeader(http.StatusInternalServerError)
-		w.Write([]byte("error marhalling json"))
+		w.Write([]byte("error marshalling json"))
 		return
 	}
 	w.WriteHeader(http.StatusOK)
