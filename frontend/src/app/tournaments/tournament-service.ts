3917410441a33c2fe338cb8e06d8a37b762f3ee3
IDOMATH <btdothemath@gmail.com>
2025-03-14 21:08:16 -0500

    Stubbing out tournament service

--- frontend/src/app/tournaments/tournament-service.ts
+++ frontend/src/app/tournaments/tournament-service.ts
@@ -3,5 +3,14 @@ import { Tournament } from "./tournament.model";
 
 @Injectable({ providedIn: "root" })
 export class TournamentService {
-  getTournamentById() {}
+  getTournamentById(id: number) {}
+  getTournaments() {}
+  postTournament(tournament: Tournament) {
+    JSON.stringify(tournament);
+    // call to post tournament
+  }
+  putTournament(tournament: Tournament) {
+    JSON.stringify(tournament);
+  }
+  deleteTournament(id: number) {}
 }
