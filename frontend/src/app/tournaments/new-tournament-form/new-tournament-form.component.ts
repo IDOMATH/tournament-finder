df14c49ba957ec441c5a39d454f253a02b827b7d
IDOMATH <btdothemath@gmail.com>
2025-04-30 21:44:43 -0500

    Starting conversion to reactive form

--- frontend/src/app/tournaments/new-tournament-form/new-tournament-form.component.ts
+++ frontend/src/app/tournaments/new-tournament-form/new-tournament-form.component.ts
@@ -1,14 +1,23 @@
 import { Component } from "@angular/core";
-import { FormsModule } from "@angular/forms";
+import {
+  FormControl,
+  FormGroup,
+  FormsModule,
+  ReactiveFormsModule,
+} from "@angular/forms";
 import { Tournament } from "../tournament.model";
 
 @Component({
   selector: "app-new-tournament-form",
-  imports: [FormsModule],
+  imports: [ReactiveFormsModule],
   templateUrl: "./new-tournament-form.component.html",
   styleUrl: "./new-tournament-form.component.css",
 })
 export class NewTournamentFormComponent {
+  form = new FormGroup({
+    name: new FormControl("", {}),
+    locationName: new FormControl("", {}),
+  });
   enteredTournamentName = "";
   enteredLocationName = "";
   enteredStreetAddress = "";
@@ -26,23 +35,6 @@ export class NewTournamentFormComponent {
   enteredIsGirlsYouth = false;
 
   onSubmit() {
-    let tournament: Tournament = {
-      name: this.enteredTournamentName,
-      locationName: this.enteredLocationName,
-      streetAddress: this.enteredStreetAddress,
-      city: this.enteredCity,
-      state: this.enteredState,
-      startDate: new Date(this.enteredStartDate),
-      endDate: new Date(this.enteredEndDate),
-      isBoysVarsity: this.enteredIsBoysVarsity,
-      isGirlsVarsity: this.enteredIsGirlsVarsity,
-      isBoysJv: this.enteredIsBoysJv,
-      isGirlsJv: this.enteredIsGirlsJv,
-      isBoysMs: this.enteredIsBoysMs,
-      isGirlsMs: this.enteredIsGirlsMs,
-      isBoysYouth: this.enteredIsBoysYouth,
-      isGirlsYouth: this.enteredIsGirlsYouth,
-    };
     console.log("submitting");
     console.log(this.enteredIsBoysVarsity);
   }
