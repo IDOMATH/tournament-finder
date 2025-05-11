3f51f138dc73d21db97957d5a712c470d7dbd867
IDOMATH <btdothemath@gmail.com>
2025-05-10 21:08:38 -0500

    Adding some date piping

--- frontend/src/app/tournaments/tournament/tournament.component.ts
+++ frontend/src/app/tournaments/tournament/tournament.component.ts
@@ -1,10 +1,11 @@
 import { Component, inject, signal } from "@angular/core";
 import { TournamentService } from "../../services/tournament-service";
 import { Tournament } from "../../models/tournament.model";
+import { DatePipe } from "@angular/common";
 
 @Component({
   selector: "app-tournament",
-  imports: [],
+  imports: [DatePipe],
   templateUrl: "./tournament.component.html",
   styleUrl: "./tournament.component.css",
 })
