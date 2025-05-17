fb9997e5313ec467d63c07384d82d91caeed66d2
IDOMATH <btdothemath@gmail.com>
2025-05-16 20:28:21 -0500

    Adding some availability text

--- frontend/src/app/age-division/age-division.component.ts
+++ frontend/src/app/age-division/age-division.component.ts
@@ -7,5 +7,20 @@ import { Component, input } from "@angular/core";
   styleUrl: "./age-division.component.css",
 })
 export class AgeDivisionComponent {
-  ageDivision = input.required<number>();
+  ageDivision = input.required<string>();
+  availability = input.required<number>();
+
+  getAvailabilityText() {
+    if (this.availability() === 0) {
+      return "not offered.";
+    }
+    if (this.availability() === 1) {
+      return "taking registrations.";
+    }
+    if (this.availability() === 2) {
+      return "is full.";
+    }
+
+    return "has an error.  Let us know how you found this.";
+  }
 }
