a49857066e665d5fb151921c7142b2cd782445bc
IDOMATH <btdothemath@gmail.com>
2025-06-04 21:28:10 -0500

    Trying out more token stuff

--- backend/go.mod
+++ backend/go.mod
@@ -7,3 +7,5 @@ require (
 	github.com/lib/pq v1.10.9
 	golang.org/x/crypto v0.38.0
 )
+
+require github.com/IDOMATH/CheetahMath v0.0.0-20250604015506-f5328303d328 // indirect
--- backend/repository/repository.go
+++ backend/repository/repository.go
@@ -6,10 +6,12 @@ import (
 	"strconv"
 	"time"
 
+	"github.com/IDOMATH/CheetahMath/formulas"
 	"github.com/IDOMATH/session/memorystore"
 	"github.com/IDOMATH/tournament-finder/constants"
 	"github.com/IDOMATH/tournament-finder/db"
 	"github.com/IDOMATH/tournament-finder/types"
+	"github.com/IDOMATH/tournament-finder/util"
 )
 
 type Repository struct {
@@ -28,10 +30,15 @@ func (repo *Repository) HandleLogin(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	stringToHash := strconv.Itoa(id) + time.Now().String()
+	// var str strings.Builder
+	// str.WriteString(strconv.Itoa(id))
+	// str.WriteString(strconv.Itoa(int(time.Now().UnixNano())))
+	t := int(time.Now().UnixMilli())*formulas.IntPow(10, formulas.GetDigits(id)) + id
+
+	token := util.TenToThirtysix(t)
 	// token := sha256.Sum256([]byte(stringToHash))
 	// repo.Session.Insert(string(token[:]), []byte(strconv.Itoa(id)), time.Now().Add(time.Hour))
 	// w.Header().Set(constants.AuthToken, string(token[:]))
-	repo.Session.Insert(stringToHash, []byte(strconv.Itoa(id)), time.Now().Add(time.Hour))
-	w.Header().Set(constants.AuthToken, stringToHash)
+	repo.Session.Insert(token, []byte(strconv.Itoa(id)), time.Now().Add(time.Hour))
+	w.Header().Set(constants.AuthToken, token)
 }
