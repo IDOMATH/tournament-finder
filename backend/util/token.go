903127fedc29a2bd11c6fb62bc8a3d580b0b6926
IDOMATH <btdothemath@gmail.com>
2025-06-23 21:32:57 -0500

    Finishing up the current token utils and some tests

--- backend/util/token.go
+++ backend/util/token.go
@@ -10,7 +10,7 @@ import (
 
 // time.Now().UnixMicro() won't add another digit for about 260 years
 // so this is very safe because nobody will be using this when that happens.
-const unixMicroDigits = 16
+const unixMilliDigits = 13
 
 func IntifyId(token []byte) int {
 	id, err := strconv.Atoi(string(token[:]))
@@ -21,21 +21,21 @@ func IntifyId(token []byte) int {
 	return id
 }
 
+// This will work as long as we have less than 1,000,000 users.
+// We can celebrate if we ever have to figure out how to work around that
 func MakeToken(id int) string {
 	id64 := int64(id)
 
-	t := time.Now().UnixMicro()*formulas.IntPow(10, int64(formulas.GetDigits(id64))) + id64
+	t := time.Now().UnixMilli()*formulas.IntPow(10, int64(formulas.GetDigits(id64))) + id64
 	return TenToThirtysix(t)
 
 }
 
-func GetUserIdFromToken(token []byte) int64 {
+func GetUserIdFromToken(token string) int {
 	// token -> base 10
-	var baseTen int64 = ThirtysixToTen(string(token[:]))
+	var baseTen int64 = ThirtysixToTen(token)
 	// remove leading unixMilli()
-	leftOver := int64(formulas.GetDigits(baseTen) - unixMicroDigits)
-	time.Now().UnixMicro()
-	return baseTen % formulas.IntPow(10, leftOver)
-
-	// should be left with Id
+	leftOver := int64(formulas.GetDigits(baseTen) - unixMilliDigits)
+	id := baseTen % formulas.IntPow(10, leftOver)
+	return int(id)
 }
--- backend/util/token_test.go
+++ backend/util/token_test.go
@@ -1,1 +1,46 @@
 package util
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestTokenId(t *testing.T) {
+	expected := 17
+	token := MakeToken(expected)
+	got := GetUserIdFromToken(token)
+	if got != expected {
+		t.Error("Expected ", expected, ", got ", got)
+	}
+
+	expected = 983
+	token = MakeToken(expected)
+	got = GetUserIdFromToken(token)
+	if got != expected {
+		t.Error("Expected ", expected, ", got ", got)
+	}
+
+	expected = 2983
+	token = MakeToken(expected)
+	fmt.Println(token)
+	got = GetUserIdFromToken(token)
+	if got != expected {
+		t.Error("Expected ", expected, ", got ", got)
+	}
+
+	expected = 629834
+	token = MakeToken(expected)
+	fmt.Println(token)
+	got = GetUserIdFromToken(token)
+	if got != expected {
+		t.Error("Expected ", expected, ", got ", got)
+	}
+
+	expected = 999999
+	token = MakeToken(expected)
+	fmt.Println(token)
+	got = GetUserIdFromToken(token)
+	if got != expected {
+		t.Error("Expected ", expected, ", got ", got)
+	}
+}
