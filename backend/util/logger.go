fd6a9833db06f21e794c6049fb7edb25843b3614
IDOMATH <btdothemath@gmail.com>
2025-08-16 08:46:31 -0500

    Playing around wiht logger

--- backend/util/logger.go
+++ backend/util/logger.go
@@ -13,9 +13,8 @@ func (l *Logger) LogError(method, message string) {
 	msg := fmt.Sprintf("ERROR in method: %s - %s", method, message)
 	fmt.Println(msg)
 	// Write to some log location
-	f, err := os.Create(l.LogLocation)
+	err := os.WriteFile(l.LogLocation, []byte(msg), 0644)
 	if err != nil {
 		fmt.Println(err)
 	}
-	defer f.Close()
 }
