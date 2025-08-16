d5294dad32086ec032b4e035d39ac6dafddf76a4
IDOMATH <btdothemath@gmail.com>
2025-08-15 22:48:01 -0500

    Continuing error logger

--- backend/util/logger.go
+++ backend/util/logger.go
@@ -1,6 +1,9 @@
 package util
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
 type Logger struct {
 	LogLocation string
@@ -10,4 +13,9 @@ func (l *Logger) LogError(method, message string) {
 	msg := fmt.Sprintf("ERROR in method: %s - %s", method, message)
 	fmt.Println(msg)
 	// Write to some log location
+	f, err := os.Create(l.LogLocation)
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer f.Close()
 }
