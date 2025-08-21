3db05bf3e209aec7921defe6120390a3159a77bd
IDOMATH <btdothemath@gmail.com>
2025-08-21 17:26:46 -0500

    user service

--- frontend/src/app/auth/login/login.component.ts
+++ frontend/src/app/auth/login/login.component.ts
@@ -1,5 +1,6 @@
-import { Component } from "@angular/core";
+import { Component, inject } from "@angular/core";
 import { FormControl, FormGroup, Validators } from "@angular/forms";
+import { TournamentService, UserService } from "../../services/user-service";
 
 @Component({
   selector: "app-login",
@@ -8,6 +9,7 @@ import { FormControl, FormGroup, Validators } from "@angular/forms";
   styleUrl: "./login.component.css",
 })
 export class LoginComponent {
+  private tournamentService = inject(UserService);
   form = new FormGroup({
     email: new FormControl({ validators: [Validators.required] }),
     password: new FormControl({ validators: [Validators.required] }),
