b8aa5f2190531da2342ddec6caebe44c65b7d94e
IDOMATH <btdothemath@gmail.com>
2025-08-20 20:17:56 -0500

    Login validator

--- frontend/src/app/auth/login/login.component.ts
+++ frontend/src/app/auth/login/login.component.ts
@@ -1,5 +1,23 @@
 import { Component } from "@angular/core";
-import { FormControl, FormGroup, Validators } from "@angular/forms";
+import {
+  AbstractControl,
+  FormControl,
+  FormGroup,
+  Validators,
+} from "@angular/forms";
+
+function valuesMatch(controlName1: string, controlName2: string) {
+  return (control: AbstractControl) => {
+    const val1 = control.get(controlName1)?.value;
+    const val2 = control.get(controlName2)?.value;
+
+    if (val1 === val2) {
+      return null;
+    }
+
+    return { passwordsDoNotMatch: true };
+  };
+}
 
 @Component({
   selector: "app-login",
