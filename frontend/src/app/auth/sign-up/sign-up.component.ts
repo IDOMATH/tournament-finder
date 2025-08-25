f17330969c5bda6f16a51b0f3d536fe482323e15
IDOMATH <btdothemath@gmail.com>
2025-08-24 20:37:26 -0500

    Signup form

--- frontend/src/app/auth/sign-up/sign-up.component.ts
+++ frontend/src/app/auth/sign-up/sign-up.component.ts
@@ -15,8 +15,8 @@ export class SignUpComponent {
     email: new FormControl("", { validators: [Validators.required] }),
     passwords: new FormGroup(
       {
-        password: new FormControl({ validators: [Validators.required] }),
-        cPassword: new FormControl({ validators: [Validators.required] }),
+        password: new FormControl("", { validators: [Validators.required] }),
+        cPassword: new FormControl("", { validators: [Validators.required] }),
       },
       { validators: [valuesMatch("password", "cPassword")] }
     ),
