a676a307a020e6096a3f2c56372d1e92477b35a7
IDOMATH <btdothemath@gmail.com>
2025-09-24 17:19:21 -0500

    Starting to integrate front and back

--- frontend/src/app/services/tournament-service.ts
+++ frontend/src/app/services/tournament-service.ts
@@ -1,34 +1,19 @@
-import { inject, Injectable } from "@angular/core";
+import { inject, Injectable, signal } from "@angular/core";
 import { Tournament } from "../models/tournament.model";
 import { HttpClient } from "@angular/common/http";
+import { map } from "rxjs";
 
 @Injectable({ providedIn: "root" })
 export class TournamentService {
   private httpClient = inject(HttpClient);
+  tournament = signal<Tournament | undefined>(undefined);
 
-  getTournamentById(id: number): Tournament {
-    this.httpClient.get("http://localhost:8080/tournaments/" + id);
-    const tournament = {
-      name: "Test Tournament",
-      locationName: "Joe's House",
-      streetAddress: "123 Small St",
-      city: "St. Paul",
-      state: "Minnesota",
-      startDate: new Date(2025, 11, 17),
-      endDate: new Date(2025, 11, 17),
-      boysVarsity: 2,
-      girlsVarsity: 2,
-      boysJv: 1,
-      girlsJv: 1,
-      boysMs: 0,
-      girlsMs: 0,
-      boysYouth: 0,
-      girlsYouth: 0,
-      id: id,
-      organizerId: 0,
-    };
-
-    return tournament;
+  getTournamentById(id: number) {
+    return this.httpClient
+      .get<{ tournament: Tournament }>(
+        "http://localhost:8080/tournaments/" + id
+      )
+      .pipe(map((resData) => this.tournament.set(resData.tournament)));
   }
   getTournaments(): Tournament[] {
     this.httpClient.get("http://localhost:8080/tournaments/");
