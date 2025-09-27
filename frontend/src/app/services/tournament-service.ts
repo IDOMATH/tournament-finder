0a98809da0827a0de085392eea19ff8ff8d58e9f
IDOMATH <btdothemath@gmail.com>
2025-09-26 21:44:45 -0500

    Wires

--- frontend/src/app/services/tournament-service.ts
+++ frontend/src/app/services/tournament-service.ts
@@ -1,12 +1,22 @@
 import { inject, Injectable, signal } from "@angular/core";
 import { Tournament } from "../models/tournament.model";
 import { HttpClient } from "@angular/common/http";
-import { map } from "rxjs";
+import { map, tap } from "rxjs";
+import { outputToObservable } from "@angular/core/rxjs-interop";
 
 @Injectable({ providedIn: "root" })
 export class TournamentService {
   private httpClient = inject(HttpClient);
-  tournament = signal<Tournament | undefined>(undefined);
+  private tournament = signal<Tournament | undefined>(undefined);
+  private tournaments = signal<Tournament[]>([]);
+
+  setFetchedTournaments() {
+    return this.getTournaments().pipe(
+      tap({
+        next: (tournaments) => this.tournaments.set(tournaments),
+      })
+    );
+  }
 
   getTournamentById(id: number) {
     return this.httpClient
@@ -15,53 +25,13 @@ export class TournamentService {
       )
       .pipe(map((resData) => this.tournament.set(resData.tournament)));
   }
-  getTournaments(): Tournament[] {
-    this.httpClient.get<{ tournaments: Tournament[] }>(
-      "http://localhost:8080/tournaments/"
-    );
-    const tournaments = [
-      {
-        name: "Test One",
-        locationName: "Joe's House",
-        streetAddress: "123 Small St",
-        city: "St. Paul",
-        state: "Minnesota",
-        startDate: new Date(2025, 11, 17),
-        endDate: new Date(2025, 11, 17),
-        boysVarsity: 2,
-        girlsVarsity: 2,
-        boysJv: 1,
-        girlsJv: 1,
-        boysMs: 0,
-        girlsMs: 0,
-        boysYouth: 0,
-        girlsYouth: 0,
-        id: 17,
-        organizerId: 0,
-      },
-      {
-        name: "Test TOO",
-        locationName: "Shakopee High School",
-        streetAddress: "123 Small St",
-        city: "Shakopee",
-        state: "Minnesota",
-        startDate: new Date(2025, 11, 17),
-        endDate: new Date(2025, 11, 17),
-        boysVarsity: 2,
-        girlsVarsity: 2,
-        boysJv: 1,
-        girlsJv: 1,
-        boysMs: 0,
-        girlsMs: 0,
-        boysYouth: 0,
-        girlsYouth: 0,
-        id: 24,
-        organizerId: 0,
-      },
-    ];
 
-    return tournaments;
+  getTournaments() {
+    return this.httpClient
+      .get<{ tournaments: Tournament[] }>("http://localhost:8080/tournaments/")
+      .pipe(map((resData) => resData.tournaments));
   }
+
   postTournament(tournament: Tournament) {
     JSON.stringify(tournament);
     // call to post tournament
