eb8a3ba4558e2dfff1d0e7badd012093afb1d396
IDOMATH <btdothemath@gmail.com>
2025-10-05 20:45:52 -0500

    Morerror

--- frontend/src/app/services/user-service.ts
+++ frontend/src/app/services/user-service.ts
@@ -1,11 +1,19 @@
 import { HttpClient } from "@angular/common/http";
 import { inject, Injectable } from "@angular/core";
+import { catchError } from "rxjs";
 
 @Injectable({ providedIn: "root" })
 export class UserService {
   private httpClient = inject(HttpClient);
   signUp(email: string, password: string) {
-    this.httpClient.post("http://localhost:8080/users/", { email, password });
+    this.httpClient
+      .post("http://localhost:8080/users/", { email, password })
+      .pipe(
+        catchError((error) => {
+          console.log("failed to sign user up");
+          return throwError(() => new Error("failed to sign user up"));
+        })
+      );
   }
   login(email: string, password: string) {
     this.httpClient.post("http://localhost:8080/login/", { email, password });
