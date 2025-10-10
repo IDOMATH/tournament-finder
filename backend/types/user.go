fb8a9665fce4e2f252c974b127e1d4fbe04f0862
IDOMATH <btdothemath@gmail.com>
2025-10-09 21:13:58 -0500

    Coach/organizer

--- backend/types/user.go
+++ backend/types/user.go
@@ -15,7 +15,9 @@ type LoginFormUser struct {
 }
 
 type NewUser struct {
-	Email    string `json:"email"`
-	Password string `json:"password"`
-	Name     string `json:"name"`
+	Email       string `json:"email"`
+	Password    string `json:"password"`
+	Name        string `json:"name"`
+	IsOrganizer string `json:"isOrganizer"`
+	IsCoach     bool   `json:"isCoach"`
 }
