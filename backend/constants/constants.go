1314ff3bcef20cd5bfb5df6302a1440bf3678225
IDOMATH <btdothemath@gmail.com>
2025-11-02 23:03:29 -0600

    Update constants.go

--- backend/constants/constants.go
+++ backend/constants/constants.go
@@ -1,4 +1,4 @@
 package constants
 
 const EmailInAlreadyInUse string = "pq: duplicate key value violates unique constraint \"users_email_key\""
-const AuthToken string = "cheetauth"
+const AuthToken string = "Cheetauth"
