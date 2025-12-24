29f794b7d2091d305a9e2f0bb20dc7d2b314e8b6
IDOMATH <btdothemath@gmail.com>
2025-12-23 21:36:53 -0600

    lgs

--- backend/repository/user_handler.go
+++ backend/repository/user_handler.go
@@ -44,6 +44,7 @@ func (repo *Repository) HandlePostNewUser(w http.ResponseWriter, r *http.Request
 	var user types.NewUser
 	err := json.NewDecoder(r.Body).Decode(&user)
 	if err != nil {
+		log.Error("HandlePostNewUser", "error decoding user to json from body", err)
 		w.WriteHeader(http.StatusInternalServerError)
 		w.Write([]byte("getting user from body"))
 		return
